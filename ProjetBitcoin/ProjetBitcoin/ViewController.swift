0a62f794c31bd4ee92678fe97a0aee8db8a3b2c2
Justine <justine.sgherzi@gmail.com>
2019-01-22 13:37:35 +0100

    ajout UI

--- ProjetBitcoin/ProjetBitcoin/ViewController.swift
+++ ProjetBitcoin/ProjetBitcoin/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker1: UIDatePicker!
+    @IBOutlet weak var datePicker2: UIDatePicker!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var listButton: UIButton!
+    @IBOutlet weak var graphButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func valueChangedDatePicker1(_ sender: Any) {
+    }
+    
+    @IBAction func valueChangedDatePicker2(_ sender: Any) {
+    }
+    
+    @IBAction func tapListButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapGraphButton(_ sender: Any) {
+    }
 }
 
